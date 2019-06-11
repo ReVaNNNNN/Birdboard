36412c9e34960e4dd7ac25d242e0390fbdf2bd28
ReVaN <revannn5480@gmail.com>
2019-06-11 08:39:58 +0200

    Reconfigured tailwind and add some style to projects view.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,15 +2,19 @@
 
 @section('content')
     <div class="flex items-center mb-4">
-        <h1 class="mr-auto">Birdboard</h1>
         <a href="{{ route('projects-create') }}">New Project</a>
     </div>
-    @forelse($projects as $project)
-        <li>
-            <a href="{{ $project->path() }}"> {{ $project->title }} </a>
-        </li>
-    @empty
-        <li>No projects yet.</li>
-    @endforelse
+
+    <div class="flex">
+        @forelse($projects as $project)
+            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
+                <h3 class="font-normal text-xl mb-6 py-4"> {{ $project->title }} </h3>
+
+                <div class="text-grey"> {{ str_limit($project->description, 100) }} </div>
+            </div>
+        @empty
+            <div>No projects yet.</div>
+        @endforelse
+    </div>
 @endsection
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -18,5 +18,5 @@ mix.js('resources/js/app.js', 'public/js');
 mix.sass('resources/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false,
-        postCss: [ tailwindcss('./tailwind.config.js') ],
+        postCss: [ tailwindcss('./tailwind.js') ],
     });
