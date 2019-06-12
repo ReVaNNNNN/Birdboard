19b28d70d5b41102e6144653e23e5e21b7205417
ReVaN <revannn5480@gmail.com>
2019-06-12 19:48:42 +0200

    Another view changes, styled projects divs.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,20 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center mb-4">
-        <a href="{{ route('projects-create') }}">New Project</a>
-    </div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-center w-full">
+            <h2 class="text-grey text-md font-normal">My projects</h2>
+            <a href="{{ route('projects-create') }}" class="button">New Project</a>
+        </div>
+    </header>
 
-    <div class="flex">
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
-            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
-                <h3 class="font-normal text-xl mb-6 py-4"> {{ $project->title }} </h3>
+            <div class="lg:w-1/4 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px;">
+                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3  border-l-4 border-blue pl-4" style="border-left: 4px solid #47cdff;">
+                        <a href=" {{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+                    </h3>
 
-                <div class="text-grey"> {{ str_limit($project->description, 100) }} </div>
+                    <div class="text-grey">{{ str_limit($project->description, 200) }}</div>
+                </div>
             </div>
         @empty
             <div>No projects yet.</div>
         @endforelse
-    </div>
+    </main>
 @endsection
 
