f9db93647262f95bf90fb5536fe197cd16925ea9
ReVaN <revannn5480@gmail.com>
2019-07-30 07:10:23 +0200

    Created new class - ActivityTest.

---
+++ tests/Unit/ActivityTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Project;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ActivityTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function it_has_a_user()
+    {
+        $project = factory(Project::class);
+
+        $this->assertInstanceOf(User::class, $project->activity->first()->user);
+    }
+}
