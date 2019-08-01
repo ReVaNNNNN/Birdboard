f8667c6f80295f8a66527e05ecf29df620507b77
ReVaN <revannn5480@gmail.com>
2019-08-01 08:01:28 +0200

    Added user as owner for each task - refactoring.

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -2,8 +2,7 @@
 
 namespace Tests\Unit;
 
-use App\Project;
-use App\User;
+use Facades\Tests\Setup\ProjectFactory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -13,10 +12,10 @@ class ActivityTest extends TestCase
 
     public function test_it_has_a_user()
     {
-        // $this->signIn();
+        $user = $this->sigIn();
 
-        $project = factory(Project::class)->create();
+        $project = ProjectFactory::ownedBy($user)->create();
 
-        $this->assertInstanceOf(User::class, $project->activity->first()->user);
+        $this->assertEquals($user->id, $project->activity->first()->user->id);
     }
 }
