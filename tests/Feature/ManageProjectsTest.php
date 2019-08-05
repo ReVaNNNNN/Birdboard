814cc961052f5578e8fb6d35688d85e26d57f0de
ReVaN <revannn5480@gmail.com>
2019-08-05 08:15:51 +0200

    |Inviting a users to projects - implementation part 2.

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -143,4 +143,16 @@ class ManageProjectsTest extends TestCase
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
+
+    public function test_user_can_see_all_projects_they_have_been_invited_to_on_their_dashboard()
+    {
+        $user = $this->sigIn();
+
+        $project = ProjectFactory::create();
+
+        $project->invite($user);
+
+        $this->get('/projects')
+            ->assertSee($project->title);
+    }
 }
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Unit;
 
 
+use App\User;
 use Illuminate\Database\Eloquent\Collection;
+use phpDocumentor\Reflection\ProjectFactory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -17,5 +19,20 @@ class UserTest extends TestCase
         $user = factory('App\User')->create();
 
         $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+
+    public function test_user_has_accessible_projects()
+    {
+        $john = $this->sigIn();
+
+        $project = ProjectFactory::ownedBy($john)->create();
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $sally = factory(User::class)->create();
+
+        ProjectFactory::ownedBy($sally)->create()->invite($john);
+
+        $this->assertCount(2, $john->accessibleProjects());
     }
 }
