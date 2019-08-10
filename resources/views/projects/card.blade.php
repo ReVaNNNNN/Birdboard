4542b17b8f8d25fe2db3208ab5bdfb77faf9ff81
ReVaN <revannn5480@gmail.com>
2019-08-10 16:12:27 +0200

    Inviting users - creating button in project.

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,9 +1,9 @@
-<div class="card" style="height: 200px;">
+<div class="card flex flex-col" style="height: 200px;">
     <h3 class="font-normal text-xl py-4 -ml-2 mb-3 pl-4" style="border-left: 4px solid #47cdff;">
         <a href=" {{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
     </h3>
 
-    <div class="text-grey mb-4">{{ str_limit($project->description, 200) }}</div>
+    <div class="text-grey mb-4 flex-1">{{ str_limit($project->description, 200) }}</div>
 
     <footer>
         <form method="POST" action="{{ route('projects-destroy', $project->id) }}" class="text-right">
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -87,8 +87,25 @@
 
             <div class="lg:w-1/4 px-3 lg:py-8">
                 @include('projects.card')
-
                 @include('projects.activity.card')
+
+                <div class="card flex flex-col mt-3" style="height: 200px;">
+                    <h3 class="font-normal text-xl py-4 -ml-2 mb-3 pl-4" style="border-left: 4px solid #47cdff;">
+                        Invite a User
+                    </h3>
+
+                    <form method="POST" action="{{ route('invitations-store', $project->id) }}" >
+                        @csrf
+
+                        <div class="mb-3">
+                            <input type="email" name="email" class="border border-grey rounded w-full py-2 px-3" placeholder="Email adress">
+                        </div>
+
+                        <button type="submit" class="button">
+                            Invite
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     </main>
