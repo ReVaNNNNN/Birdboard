a0c1d59b61251982b5cac9a307e508fdb469eb8b
ReVaN <revannn5480@gmail.com>
2019-08-21 17:50:48 +0200

    Added new theme - added new Vue component: ThemeSwitcher - added advanced handling of switch buttons.

--- resources/js/components/ThemeSwitcher.vue
+++ resources/js/components/ThemeSwitcher.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="flex items-center mr-8">
-        <button class="rounded-full w-4 h-4 bg-default border border-accent mr-2" @click="selectedTheme = 'theme-light'"></button>
-        <button class="rounded-full w-4 h-4 bg-default border border-accent mr-2" @click="selectedTheme = 'theme-dark'"></button>
+        <button v-for="(color, theme) in themes" class="rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none"
+                :class="{ 'border-accent' : selectedTheme == 'theme'}"
+                :style="{background: color}"
+                @click="selectedTheme = theme"
+        ></button>
     </div>
 </template>
 
@@ -9,13 +12,23 @@
     export default {
         data() {
             return {
+                themes: {
+                    'theme-light' : '#F5F6F9',
+                    'theme-dark' : '#222'
+                },
                 selectedTheme: 'theme-light'
             }
         },
 
+        created() {
+            this.selectedTheme = localStorage.getItem('theme') || 'theme-light';
+        },
+
         watch: {
             selectedTheme() {
-                alert('changed');
+                document.body.className = document.body.className.replace(/theme-\w+/, this.selectedTheme);
+
+                localStorage.setItem('theme', this.selectedTheme);
             }
         }
     }
